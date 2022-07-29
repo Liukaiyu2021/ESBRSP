453583b558a4b0d6910b4bafb5b9cecffa9315d6
Administrator <3984756@qq.com>
2022-07-29 23:44:01 +0800

    Default Changelist

---
+++ src/main/java/com/paladin/esbrsp/EsbrspApplication.java
@@ -0,0 +1,13 @@
+package com.paladin.esbrsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbrspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsbrspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paladin/esbrsp/EsbrspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paladin.esbrsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbrspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
